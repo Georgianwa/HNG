77c75b79cceeb69d13b4bb2feee29607ca4cd17b
Georgia Nwa <georgia.u.nwa@gmail.com>
2025-01-31 05:09:52 +0100

    dgfhvjh

--- Backend/week 1/server.js
+++ Backend/week 1/server.js
@@ -4,10 +4,3 @@ const app = require("./index");
 app.listen(3000, () => {
     console.log("Express server live on port 3000");
 });
-
-const fetch = require("node-fetch");
-
-fetch("https://hng-bel9.onrender.com/")
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.log(error));
